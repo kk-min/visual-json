ea4a79e6296a31161c16a574e792564cf5db80eb
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-04 02:58:01 +0800

    Update Visualizer component

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,8 +1,9 @@
 import { useFileStore } from '../data/filestore';
+import Node from './Node';
 
 export default function Visualizer() {
 	const file = useFileStore((state: any) => state.file);
 	const json = file ? JSON.parse(file) : null;
 
-	return ()
+	return (<Node {...json} />)
 }
