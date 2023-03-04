716f0b7ea3b1e6919a249c9a07de884335abd966
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-04 10:56:02 +0800

    Update JSON parsing in Visualizer.tsx

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,9 +1,15 @@
 import { useFileStore } from '../data/filestore';
 import Node from './Node';
 
+const fileReader = new FileReader();
 export default function Visualizer() {
 	const file = useFileStore((state: any) => state.file);
-	const json = file ? JSON.parse(file) : null;
-
-	return (<Node {...json} />)
+	if (!file) return null;
+	fileReader.readAsText(file, 'UTF-8');
+	fileReader.onload = (e: any) => {
+		const content = e.target.result;
+		const json = JSON.parse(content);
+		return (<Node {...json} />)
+	}
+	return null;
 }
