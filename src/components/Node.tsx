b78c732e6b5358555f07256b96711ef990dcc156
kk-min <76023265+kk-min@users.noreply.github.com>
2023-03-05 20:53:51 +0800

    Minor refactor

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -9,23 +9,17 @@ export default function Node(props: NodeProps) {
 	if (jsonObj === null) {
 		return <div className="Node">NULL</div>;
 	}
-	let level = [];
-	for (const key in jsonObj) {
-		if (jsonObj.hasOwnProperty(key)) {
-			let element = jsonObj[key as keyof JSON]!;
-			level.push({ key, element });
-		}
-	}
+	let level = Object.entries(jsonObj)
 	return <div className="Level">{
-		level.map((item) => {
-			if (typeof item.element === 'object') {
+		level.map(([key, value]) => {
+			if (typeof value === 'object') {
 				return <div >
-					<div className="Node">{Array.isArray(item.element) ? "[ " : ""}{`"${item.key}"`}{Array.isArray(item.element) ? " ]" : ""}</div>
-					{Array.isArray(item.element) ? <div className="Level">{(item.element as Array<any>).map((subitem) => (typeof subitem === 'object') ? <div ><div className="Node">{Array.isArray(subitem) ? "[ " : "{ "}{"_"}{Array.isArray(subitem) ? " ]" : " }"}</div><Node jsonObj={subitem} /></div> : <div className="Node">{typeof subitem === "string" ? `"${subitem}"` : subitem}</div>)}</div> : <Node jsonObj={item.element} />}
+					<div className="Node">{Array.isArray(value) ? "[ " : ""}{`"${key}"`}{Array.isArray(value) ? " ]" : ""}</div>
+					{Array.isArray(value) ? <div className="Level">{(value as Array<any>).map((subitem) => (typeof subitem === 'object') ? <div ><div className="Node">{Array.isArray(subitem) ? "[ " : "{ "}{"_"}{Array.isArray(subitem) ? " ]" : " }"}</div><Node jsonObj={subitem} /></div> : <div className="Node">{typeof subitem === "string" ? `"${subitem}"` : subitem}</div>)}</div> : <Node jsonObj={value} />}
 				</div>;
 			} else {
-				const element = typeof item.element === "string" ? `"${item.element}"` : item.element;
-				return <div className="Node">{`"${item.key}"` + " : " + element}</div>;
+				const displayValue = typeof value === "string" ? `"${value}"` : value;
+				return <div className="Node">{`"${key}"` + " : " + displayValue}</div>;
 			}
 		})
 	}</div >;
